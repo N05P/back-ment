ed89ea8bc7eeb6fdfa15ec3b46a63fb4cb1ad4e1
N05P <pn545927@gmail.com>
2026-02-13 23:51:50 +0530

    fix dashboard  build

--- app/(root)/dashboard/layout.js
+++ app/(root)/dashboard/layout.js
@@ -1,6 +1,8 @@
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { getAuth } from "@/lib/better-auth/auth";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default async function RootLayout({ children }) {
     const auth = await getAuth();
