184c5583a53a6ec199d5809cb71699213d1126b3
N05P <pn545927@gmail.com>
2026-02-13 23:45:38 +0530

    fix login build

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,6 +1,8 @@
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { getAuth } from "@/lib/better-auth/auth";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default async function AuthLayout({ children }) {
     const auth = await getAuth();
